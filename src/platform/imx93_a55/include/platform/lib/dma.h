7b8ea935aeca72884fe495e87d7c2ea9e50957c3
Laurentiu Mihalcea <laurentiu.mihalcea@nxp.com>
2023-11-24 15:21:48 +0200

    imx93_a55: include: cleanup: Remove unused definitions
    With the transition to the native Zephyr drivers and the
    timer domain, some definitions from the headers found in
    the src/platform/imx93_a55/include hierarchy are no longer
    needed. As such, this commit removes them.
    
    Signed-off-by: Laurentiu Mihalcea <laurentiu.mihalcea@nxp.com>

--- src/platform/imx93_a55/include/platform/lib/dma.h
+++ src/platform/imx93_a55/include/platform/lib/dma.h
@@ -8,18 +8,7 @@
 #ifndef __PLATFORM_LIB_DMA_H__
 #define __PLATFORM_LIB_DMA_H__
 
-/* i.MX93 uses dummy DMA and EDMA */
-#define PLATFORM_NUM_DMACS 2
-
-#define DMA_ID_EDMA2 0
-#define DMA_ID_HOST 1
-
-/* TODO: this is already defined with EDMA2_CHAN_MAX */
-#define PLATFORM_MAX_DMA_CHAN 64
-
-/* TODO: required by Zephyr DMA domain to work */
-#define dma_chan_irq_name(dma, chan) dma_irq_name(dma)
-#define dma_chan_irq(dma, chan) ((int *)(dma)->plat_data.drv_plat_data)[chan]
+/* TODO: remove me whenever possible */
 
 #endif /* __PLATFORM_LIB_DMA_H__ */
 
